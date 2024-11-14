c98304f564f31aeaad1cab7c0a6816358bf036b0
LeghamC <lelia.ghezali@efrei.net>
2024-11-14 11:41:21 +0100

    update node.h

--- include/node.h
+++ include/node.h
@@ -12,13 +12,11 @@
 #define MAXI_DEPTH 5  // 5 movements possible per phase
 #define MAXI_MOVES 9  // 9 movements available per phase
 
-#define MAXI_DEPTH 5  // 5 movements possible per phase
-#define MAXI_MOVES 9  // 9 movements available per phase
-
 /**
  * @brief structure for the node of the N-ary tree
- * @param loc : coordinates and orientation on the map
  * @param value : the node's value => the square's cost
+ * @param loc : coordinates and orientation on the map
+ * @param move : last move made to the node
  * @param depth : the node's depth within the tree => at which number of moves over MAXI_DEPTH
  * @param sons : the node's sons => next moves from the current node
  * @param nbSons : the number of node's sons => the number of remaining moves
@@ -27,13 +25,12 @@
 
 typedef struct s_node
 {
-
-    int value; // the cost of the node
-    t_localisation loc; // the position of the node
-    t_move move; // the move that have been achieved to arrive to this node
-    struct s_node *sons[MAXI_MOVES]; // the sons of the node
-    int depth; // the depth of the node, to be initialized to 0, and should not exceed 5 or 4 as a phase has 5 or 4 moves
-    int nb_children; // the number of children of the node, we will decrement it each time we are going more in depth it should be initialized to 9
+    int value;
+    t_localisation loc;
+    t_move move;
+    struct s_node *sons[MAXI_MOVES];
+    int depth;
+    int nbSons;
 } t_node, *p_node;
 
 t_node *create_node(t_localisation, int, int, int);
