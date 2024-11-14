58307d8695e122e0a295c8ff9c73150f5ee475f7
noahj <noah.jeandeau@gmail.com>
2024-11-14 13:21:15 +0100

    test of the draw function and add of the function to make better randomization

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "../include/map.h"
 #include "../include/node.h"
-
-
+#include "../include/draw.h"
+#include "../include/moves.h"
 #include "../include/loc.h"
 #include "../include/node.h"
 #include "../include/tree.h"
 
 int main() {
+    srand((unsigned)time(NULL));
     t_map map = createMapFromFile("..\\maps\\example1.map");
     printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
     for (int i = 0; i < map.y_max; i++)
@@ -33,5 +34,16 @@ int main() {
     t_localisation i_loc = loc_init(1,1,NORTH);
     p_node new_node = create_node(i_loc,1,0,9);
     printf("The cost of the initial node is : %d",new_node->value);
+
+    /*
+    printf("\n\n");
+    t_stack hand ;
+    hand = draw_hand();
+    t_move temp ;
+    for (int i = 0 ; i < 9 ; i++){
+        temp = pop(&hand);
+        printf("%s / ", getMoveAsString(temp));
+    }
+*/
     return 0;
 }
