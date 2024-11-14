9166187c5b92aadc6b04d189a8175ef6e80a2728
lipotani77 <kkoulourath@icloud.com>
2024-11-14 11:20:34 +0100

    change of node structure

--- include/node.h
+++ include/node.h
@@ -6,11 +6,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "moves.h"
 
 /**
  * @brief Structure of a node
-
- */
+*/
 typedef struct s_node
 {
     /**
@@ -21,9 +21,9 @@ typedef struct s_node
     int nbSons;
     int LS; // 5 - the depth of the node
     */
-    int cost; // the cost of the node
+    int value; // the cost of the node
     t_localisation loc; // the position of the node
-    char move[20]; // the move that led to this node
+    t_move move; // the move that have been achieved to arrive to this node
     struct s_node *sons[ACTION_SIZE]; // the sons of the node
     int depth; // the depth of the node, to be initialized to 0, and should not exceed 5 or 4 as a phase has 5 or 4 moves
     int nb_children; // the number of children of the node, we will decrement it each time we are going more in depth it should be initialized to 9
