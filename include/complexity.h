c28752f6ee2feb41945f0eee9608431bc66dc474
Chocissa315 <150431999+Chocissa315@users.noreply.github.com>
2024-11-22 17:31:23 +0100

    commit of some functions

---
+++ include/complexity.h
@@ -0,0 +1,34 @@
+#ifndef UNTITLED1_COMPLEXITY_H
+#define UNTITLED1_COMPLEXITY_H
+
+#include <time.h>
+/* General Idea
+ * we call the clock function at the beginning and at the end of the function
+ then, we substract them and divide it by CLOCKS_PER_SEC to get the processor time
+ * I will add it to the structure so it is easier to get it at the end (I guess)
+ */
+
+
+//•	The tree construction phase with time_tree
+/* time_tree
+ * t_tree tree_construction (a, b, c){
+        clock_t time_tree;
+        double time
+        time_tree = clock()
+        //description of the function
+        time_tree = (clock() - time_tree;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC
+
+
+  }
+ */
+
+
+
+//•	The search for a leaf with the minimum value among all the leaves in the tree with time leaf
+
+//•	Calculating the path from the root to this leaf ; time_path?
+
+
+
+#endif //UNTITLED1_COMPLEXITY_H
---
+++ src/complexity.c
@@ -0,0 +1,1 @@
+#include "../include/complexity.h"
