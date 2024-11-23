ea9f85e64465e5cad2436e7013d132d3d9557a6e
Chocissa315 <150431999+Chocissa315@users.noreply.github.com>
2024-11-23 19:51:53 +0100

    Time execution function with the explanation on how it works

--- include/complexity.h
+++ include/complexity.h
@@ -2,33 +2,20 @@
 #define UNTITLED1_COMPLEXITY_H
 
 #include <time.h>
-/* General Idea
- * we call the clock function at the beginning and at the end of the function
- then, we substract them and divide it by CLOCKS_PER_SEC to get the processor time
- * I will add it to the structure so it is easier to get it at the end (I guess)
+
+
+
+/*
+ * The goal is to create a function that will take in parameter a starting time and an ending time and compute the time execution based on these two values.
+ * e.g : When we will create the tree, we will put at the first line of the function making the tree a variable containing the start time.
+ * The before before last line of the function will be a new variable with the end time, the penultimate line will be the call of the clock function to compute the time execution based on the 2 arguments which will be the start and end time.
  */
 
-
-//•	The tree construction phase with time_tree
-/* time_tree
- * t_tree tree_construction (a, b, c){
-        clock_t time_tree;
-        double time
-        time_tree = clock()
-        //description of the function
-        time_tree = (clock() - time_tree;
-        double time_taken = ((double)t)/CLOCKS_PER_SEC
-
-
-  }
+/**
+ * @param start // Starting time of the function computed by clock()
+ * @param end // Ending time of the function computed by clock()
+ * @return total_time // the elapsed time of the function
  */
-
-
-
-//•	The search for a leaf with the minimum value among all the leaves in the tree with time leaf
-
-//•	Calculating the path from the root to this leaf ; time_path?
-
-
+double exec_time(clock_t start, clock_t end);
 
 #endif //UNTITLED1_COMPLEXITY_H
--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "../include/map.h"
 #include "../include/node.h"
-
-
+#include "../include/complexity.h"
+#include "../include/moves.h"
 #include "../include/loc.h"
 #include "../include/node.h"
 #include "../include/tree.h"
@@ -30,10 +30,32 @@ int main() {
     displayMap(map);
     printf("Map displayed\n");
 
+    t_localisation i_loc = loc_init(1,1,NORTH);
+    //p_node new_node = create_node(i_loc,1,0,9);
+    //printf("The cost of the initial node is : %d\n",new_node->value);
+
+
+    //t_stack hand = draw_hand();
+    //t_tree tree_of_possibility = recursive_tree_creation(map, i_loc, hand);
+   // printf("The value of the root is %d\n",tree_of_possibility.root->value);
+
+    //display_tree(tree_of_possibility.root, 0);
+
+    // test of min path
+   // t_stack  stack_test;
+    //new_node
+
+    //stack_test = createStack(10);
+    int min;
+
+
     /* let's now find the base station position */
     printf("Base station position : x = %d and y = %d\n", findBaseCoordinate(map).x, findBaseCoordinate(map).y);
     int cost;
     cost = cost_node(findBaseCoordinate(map).x, findBaseCoordinate(map).y, map);
-    printf("The cost is %d", cost);
+    printf("The cost is %d \n", cost);
+    double test_exec_t;
+    test_exec_t = exec_time(2.71828,3.14159); // I choose somme random values
+    printf("It took %lf sec to run", test_exec_t);
     return 0;
 }
