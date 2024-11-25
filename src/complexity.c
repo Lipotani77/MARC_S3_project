993923e12539b76defa12858bb839486aeaa8ad3
Chocissa315 <150431999+Chocissa315@users.noreply.github.com>
2024-11-25 10:44:48 +0100

    Amelioration of the function to compute time execution

--- src/complexity.c
+++ src/complexity.c
@@ -5,8 +5,8 @@
 double exec_time(clock_t start, clock_t end){
     double total_time; // We choose double type to have as much precision as we can.
 
-    /* since clock_t  a long  integer type, we cast the values in double to not loose precision by performing the computations
-    * We divide by CLOCkS_PER_SEC because clock() give us the CPU time consumed by the function (the number of ticks it took). By doing this, get the time in second.
+    /* since clock_t  a long  integer type, we cast the values in double to not loss of precision by performing the computations
+    * We divide by CLOCkS_PER_SEC because clock() give us the CPU time consumed by the function (the number of ticks it took). By doing this, we get the time in second.
     */
     total_time = (1000.0*(double)(end - start))/CLOCKS_PER_SEC; //Total time in milliseconds
     return total_time;
